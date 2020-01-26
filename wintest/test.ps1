a79765562e4bff159ebd91f3643939164cb66f18
Guglielmo <guglielmo.felici@yahoo.it>
2020-01-27 00:35:08 +0100

    .

--- wintest/test.ps1
+++ wintest/test.ps1
@@ -9,8 +9,15 @@ $excludes = 'test[\\/]'
 Write-Output "Testing files in $path
 Recursion limited to $depth
 Port $port"
+if ($(mycurl gopher://localhost:$port// 2>&1) -match '[Ff]ail') {
+	Write-Output "Failed to connect to server"
+	exit
+}
 if (Test-Path $compareFile) {
 	Clear-Content $compareFile
+}
+else {
+	New-Item $compareFile *>$null
 }
 if (!(Test-Path $outDir)) {
 	mkdir $outDir *>$null
@@ -26,7 +33,7 @@ $result = $(Get-ChildItem -Recurse -Depth $depth -Path $path |
 		$out = $relPath -replace '[\\/]', '_'
 		mycurl gopher://localhost:$port//$relPath --output $outDir/$out *>$null 
 		if (!$isDir) {
-			$comp = (fc.exe /b $relPath $outDir\$out 2>null) | Out-String
+			$comp = (fc.exe /b $path$relPath $outDir\$out 2>&1) | Out-String
 			Write-Output $comp | Out-File $compareFile -Append
 			return $comp -notmatch '[A-Z\d]{8}: ([A-Z\d]{2} ?){2}'
 		}
